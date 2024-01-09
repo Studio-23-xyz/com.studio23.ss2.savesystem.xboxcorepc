3a86bde31d04d38f6820eee8cb0b44b1f2e01830
Md Maruf Billa Opu <73388427+gitOpu@users.noreply.github.com>
2024-01-09 17:45:23 +0600

    v0.1.0

---
+++ Runtime/Core/XboxPCCloudSaveProvider.cs
@@ -0,0 +1,114 @@
+using System;
+using System.IO;
+using Cysharp.Threading.Tasks;
+using GameSave;
+using Studio23.SS2.AuthSystem.XboxCorePC.Core;
+using Studio23.SS2.CloudSave.Core;
+using UnityEngine;
+using UnityEngine.Events;
+using XGamingRuntime;
+
+
+namespace Studio23.SS2.SaveSystem.XboxCorePc.Core
+{
+    public class XboxPCCloudSaveProvider : AbstractCloudSaveProvider
+    {
+        private GameSaveManager _saveManager;
+        private string _filePath;
+
+        protected override UniTask<int> Initialize()
+        {
+            UniTaskCompletionSource<int> cloudSaveTaskCompletionSource= new UniTaskCompletionSource<int>();
+            _saveManager = new GameSaveManager();
+            XUserHandle currentUserHandle = GamingRuntimeManager.Instance.UserManager.m_CurrentUserData.userHandle;
+
+            if (currentUserHandle == null)
+            {
+                Debug.LogError("XUserhandle not found");
+            }
+            
+            string SCID = GamingRuntimeManager.Instance.SCID;
+            
+            _saveManager.Initialize(currentUserHandle, SCID, false, result =>
+            {
+                cloudSaveTaskCompletionSource.TrySetResult(result);
+            });
+
+
+            return cloudSaveTaskCompletionSource.Task;
+        }
+
+        
+
+        protected override async UniTask<int> UploadToCloud(string key, byte[] data)
+        {
+            UniTaskCompletionSource<int> uploadedToCloud = new UniTaskCompletionSource<int>(); 
+            var containerName = $"{key}";
+            var blobBufferName = $"{key}_blobBuffer";
+            var displayName = $"{key}{DateTime.Now}";
+            
+            Debug.Log($"Saving Container: {containerName}. blob Name: {blobBufferName}");
+
+         
+
+            _saveManager.GetOrCreateContainer(containerName,
+                result =>
+                {
+                    uploadedToCloud.TrySetResult(result);
+                    if (HR.FAILED(result))
+                    {
+                        return;
+                    } 
+                    _saveManager.SaveGame(displayName,
+                        blobBufferName,
+                        data,
+                        result =>
+                        {
+                            uploadedToCloud.TrySetResult(result);
+                        });
+                });
+
+            return await uploadedToCloud.Task;
+        }
+        protected override async UniTask<byte[]> DownloadFromCloud(string key)
+        
+        {
+            var containerName = $"{key}";
+            var blobBufferName = $"{key}_blobBuffer";
+            UniTaskCompletionSource<int> getOrCreateContainerTaskCompletionSource  = new UniTaskCompletionSource<int>(); 
+            UniTaskCompletionSource<byte[]> downloadBlobTaskCompletionSource = new UniTaskCompletionSource<byte[]>();
+            
+            Debug.Log($"Loading from  Container: {containerName}. blob Name: {blobBufferName}");
+           
+            _saveManager.GetOrCreateContainer(containerName,
+                result =>
+                {
+                    getOrCreateContainerTaskCompletionSource.TrySetResult(result);
+                    if (HR.FAILED(result))
+                    {
+                        return;
+                    } 
+                    
+                    _saveManager.LoadGame(blobBufferName, (hresult, blobs) =>
+                    {
+                        getOrCreateContainerTaskCompletionSource.TrySetResult(hresult);
+                        if (HR.FAILED(result))
+                        {
+                            return;
+                        } 
+                        Debug.Log($"Loading data buffer successful. Name: {blobs[0].Info.Name} - Size: {blobs[0].Info.Size} bytes");
+                        byte[] loadedData = blobs[0].Data;
+                        downloadBlobTaskCompletionSource.TrySetResult(loadedData);
+                    });
+                });
+
+           
+            
+            return await downloadBlobTaskCompletionSource.Task;
+        }
+
+       
+
+
+    }
+}
